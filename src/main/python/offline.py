67f0d41543ea019092598ba9afdbd0bc9e9a5dfb
sasakigao <gao_shaowen@126.com>
2017-01-16 22:07:50 +0800

    online & offline

--- src/main/python/offline.py
+++ src/main/python/offline.py
@@ -10,23 +10,14 @@ from pyspark import SparkContext
 from pyspark import SparkConf
 from pyspark.mllib.tree import DecisionTree, DecisionTreeModel
 from pyspark.mllib.util import MLUtils
-from pyspark.mllib.regression import LabeledPoint
-from pyspark.mllib.linalg import Vectors
+
 
 appName="Binary Classifier"
 # Read from
-rawDataHDFS = "file:///home/sasaki/Documents/qnlog/fixed/test"
 featuresHDFS = "file:///home/sasaki/dev/gamease/example/res/offline/features/20170110-1645"
-treeModelLoadHDFS = "file:///home/sasaki/dev/gamease/example/res/offline/tree/20170110-1645"
+treeModelPathHDFS = "file:///home/sasaki/dev/gamease/example/res/offline/tree/20170110-1645"
 # Write to
-featuresSaveHDFS = "file:///home/sasaki/model/features/"
-treeModelSaveHDFS = "file:///home/sasaki/model/tree/"
 treeMetricsPathHDFS = "file:///home/sasaki/metrics/"
-
-def trainTree(train, sc, timeStr):
-	modelTree = DecisionTree.trainClassifier(train, numClasses=2, categoricalFeaturesInfo={}, impurity='gini', maxDepth=5, maxBins=32)
-	modelTree.save(sc, treeModelSaveHDFS + timeStr)
-	return modelTree
 
 def binary_redication_metrics_output(predicationsAndLabels, metricsPath, sc):
 	predicationsAndLabels.persist()
@@ -42,35 +33,20 @@ def binary_redication_metrics_output(predicationsAndLabels, metricsPath, sc):
 	sc.parallelize(metrics, 1).saveAsTextFile(metricsPath)
 	predicationsAndLabels.unpersist()
 
-def line2Feature(line):
-	values = [float(x) for x in line.split(',')]
-	return LabeledPoint(values[0], values[1:])
 
 if __name__ == "__main__":
-	# 0 means train while 1 means offline prediction
-	mode = sys.argv[1]
     	timeformat = "%Y%m%d-%H%M"
     	timeStr = time.strftime(timeformat, time.localtime())
 
 	confSpark = SparkConf().setAppName(appName)
-    	sc = SparkContext(conf = confSpark)
-
-    	if mode == "0":
-	    	rawData = sc.textFile(rawDataHDFS)
-	    	features = rawData.map(lambda x : line2Feature(x))
-	    	splits = features.randomSplit([0.7, 0.3], 21)
-	    	train = splits[0].persist()
-	    	test = splits[1].persist()
-	    	modelTree = trainTree(train, sc, timeStr)
-	    	MLUtils.saveAsLibSVMFile(features.repartition(1), featuresSaveHDFS + timeStr)
-    	else:
-	    	test = MLUtils.loadLibSVMFile(sc, featuresHDFS)
-	    	modelTree = DecisionTreeModel.load(sc, treeModelLoadHDFS)
+    	sc = SparkContext(conf=confSpark)
+    	features = MLUtils.loadLibSVMFile(sc, featuresHDFS)
+    	modelTree = DecisionTreeModel.load(sc, treeModelPathHDFS)
 
     	# In python the mllib model seems unable to be directly used in a closure.
-    	predictions = modelTree.predict(test.map(lambda x : x.features))
-    	predicationsAndLabels = predictions.zip(test.map(lambda x : x.label)).persist()
+    	predictions = modelTree.predict(features.map(lambda x : x.features))
+    	predicationsAndLabels = predictions.zip(features.map(lambda x : x.label)).persist()
 	binary_redication_metrics_output(predicationsAndLabels, treeMetricsPathHDFS + timeStr, sc)
-	predicationsAndLabels.unpersist()
+	predicationsAndLabels.persist()
 
 	sc.stop()
